cb53b9b47c9dff6ecb529f5f8a94dcb7400301ba
HAlGhanim <imzaji98@gmail.com>
2025-04-21 21:09:09 +0300

    All tasks done except profiles

--- src/test/kotlin/com/coded/spring/ordering/ApplicationTests.kt
+++ src/test/kotlin/com/coded/spring/ordering/ApplicationTests.kt
@@ -1,13 +1,41 @@
 package com.coded.spring.ordering
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import kotlin.test.assertEquals
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
+	@Autowired
+	lateinit var restTemplate: TestRestTemplate
+
 	@Test
-	fun contextLoads() {
+	fun helloWorld() {
+		val result = restTemplate.getForEntity("/hello", String::class.java)
+		assertEquals(expected = HttpStatus.OK, actual = result?.statusCode)
+		assertEquals(expected = "Hello World", actual = result.body)
 	}
+
+	@Test
+	fun createUser() {
+		val request = mapOf(
+			"name" to "HelloUser",
+			"age" to 18,
+			"username" to "testuser",
+			"password" to "password123",
+			"role" to "USER"
+		)
+
+		val response = restTemplate.postForEntity("/users/v1/register", request, String::class.java)
+
+		assertEquals(HttpStatus.OK, response.statusCode)
+
+
+	}
+
 
 }
